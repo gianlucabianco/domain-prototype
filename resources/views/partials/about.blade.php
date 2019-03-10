fb55430bb9d47e36d7f81f0da50798e6b4a980e3
Gianluca Bianco <pi.gs.bianco@gmail.com>
2019-03-10 12:34:49 +0100

    about static, todo: clean

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,3 +1,17 @@
-<div class="">
-  <span class="blue"> sono l'about</span>
+<div class="about">
+  <div class="container">
+    <div class="row">
+      <span class="h70">Questa è</span >
+      <span class="h70">una bella frase</span >
+      <span class="h70">(e questa è la sua terza riga)</span >
+    </div>
+    <div class="row">
+      <div class="col-6">
+      </div>
+      <div class="col-6 mrgTp50">
+        <p>Questo è un bel paragrafo : Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>questa è una frase ad effetto</p>
+      </div>
+    </div>
+  </div>
 </div>
