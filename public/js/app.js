d00bbd15ef2c800cd161f448803c60d34af43eff
gianlucabianco <pi.gs.bianco@gmail.com>
2019-03-12 19:17:58 +0100

    minor styling refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -49152,8 +49152,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/gianluca/Documents/REPO/domain-prototype/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/gianluca/Documents/REPO/domain-prototype/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/domain-prototype/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/domain-prototype/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,9 @@
 <div id="header">
   <div class="title">
-    <h3>HELLO, I'M</h3>
+    <h5>HELLO, I'M</h5>
     <h1>GIAN<span>LUCA</span></h1>
-    <!-- <div class="myButton">ciao</div> -->
   </div>
-  <a class="arrowDown" href="#about"><i class="fas fa-chevron-down"></i></a>
+  <a class="arrowDown" href="#about">
+    <i class="fas fa-chevron-down"></i>
+  </a>
 </div>
