d8ede446b5435f767dee1a15e7eed1f7188eb4e7
gianlucabianco <pi.gs.bianco@gmail.com>
2019-03-13 18:39:14 +0100

    blog updated

--- resources/views/partials/projects.blade.php
+++ resources/views/partials/projects.blade.php
@@ -16,7 +16,6 @@
     </div>
   </div>
 
-
   <div class="projectCard bg2">
     <div class="container">
       <div class="row">
@@ -29,13 +28,13 @@
         <div class="projectRight col-7">
           <p>Questa è la descrizione di un progetto. (on hover)
             <br>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#" class="myButton">questo è un bottone</a>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <a href="#" class="myButton">Vai al progetto</a>
         </div>
       </div>
     </div>
   </div>
-
 
   <div class="projectCard bg3">
     <div class="container">
@@ -51,12 +50,10 @@
           <p style="color: white">Questa è la descrizione di un progetto. (on hover + overlay)
             <br>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#">questo è un bottone</a>
+            <a href="#" class="myButton">Vai al progetto</a>
         </div>
       </div>
     </div>
   </div>
-
-
 
 </div>
