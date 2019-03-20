50164c75d8b8cf77bd37b5835f01cbf323ae108f
gianlucabianco <pi.gs.bianco@gmail.com>
2019-03-20 19:31:12 +0100

    blog section ani done

--- resources/views/partials/blog.blade.php
+++ resources/views/partials/blog.blade.php
@@ -1,9 +1,11 @@
 <div id="blog">
-  <div class="container">
-    <h3>NOT ONLY CODE</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <a href="#" class="myButton">Vai al blog</a>
+  <div id="blogSection" class="container animated">
+    <div class="beforeScroll">
+      <h3>NOT ONLY CODE</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <a href="#" class="myButton">Vai al blog</a>
+    </div>
   </div>
 </div>
