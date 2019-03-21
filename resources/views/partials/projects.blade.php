5a3c3a4d66033cf1d7197b99c4e5e7487a07b08a
gianlucabianco <pi.gs.bianco@gmail.com>
2019-03-21 10:21:08 +0100

    DOM animations complete

--- resources/views/partials/projects.blade.php
+++ resources/views/partials/projects.blade.php
@@ -1,16 +1,20 @@
 <div id="projectsSection">
-  <!-- ATTENZIONE:
-  STRUTTURA DELLE CARD TEMPORANEA.
-  REFACTORING IN SEDE DI CODING DEGLI EFFETTI -->
 
   <div class="projectCard bg1">
     <div class="container">
       <div class="row">
-        <div class="projectLeft col-5">
-          <h4>
+        <div class="projectLeft col-5 animated">
+          <h4 class="beforeScroll">
             <span>Questo è il titolo</span>
             <span>di un progetto.</span>
           </h4>
+        </div>
+        <div class="projectRight col-7">
+          <p>Questa è la descrizione del paragrafo (che comprende un effetto di overlay).
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <a href="#" class="myButton">Vai al progetto</a>
         </div>
       </div>
     </div>
@@ -19,14 +23,14 @@
   <div class="projectCard bg2">
     <div class="container">
       <div class="row">
-        <div class="projectLeft col-5">
-          <h4>
+        <div class="projectLeft col-5 animated">
+          <h4 class="beforeScroll">
             <span>Questo è il titolo</span>
             <span>di un progetto.</span>
           </h4>
         </div>
         <div class="projectRight col-7">
-          <p>Questa è la descrizione di un progetto. (on hover)
+          <p>Questa è la descrizione del paragrafo (che comprende un effetto di overlay).
             <br>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
@@ -39,18 +43,18 @@
   <div class="projectCard bg3">
     <div class="container">
       <div class="row">
-        <div class="projectLeft col-5">
-          <h4>
+        <div class="projectLeft col-5 animated">
+          <h4 class="beforeScroll">
             <span>Questo è il titolo</span>
             <span>di un progetto.</span>
-            <small>(che avrà un overlay)</small>
           </h4>
         </div>
         <div class="projectRight col-7">
-          <p style="color: white">Questa è la descrizione di un progetto. (on hover + overlay)
+          <p>Questa è la descrizione del paragrafo (che comprende un effetto di overlay).
             <br>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <a href="#" class="myButton">Vai al progetto</a>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <a href="#" class="myButton">Vai al progetto</a>
         </div>
       </div>
     </div>
