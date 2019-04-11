ad85845e29d59698b87829ac8af0e4c20421ebcc
gianlucabianco <pi.gs.bianco@gmail.com>
2019-04-11 11:43:06 +0200

    _merits color test

--- resources/views/partials/merits.blade.php
+++ resources/views/partials/merits.blade.php
@@ -1,7 +1,7 @@
 
 <div id="meritsSection">
 
-  <a href="#">
+  <a href="#" class="cardColor1">
     <div class="meritCard">
       <h3>
         <span>Questo è</span>
@@ -10,7 +10,7 @@
       <p>Questa è la descrizione del paragrafo (che comprende un effetto di overlay). Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore dolore dolore dolore dolore ...</p>
     </div>
   </a>
-  <a href="#">
+  <a href="#" class="cardColor2">
     <div class="meritCard">
       <h3>
         <span>Questo è</span>
@@ -19,7 +19,7 @@
       <p>Questa è la descrizione del paragrafo (che comprende un effetto di overlay). Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore dolore dolore dolore dolore ...</p>
     </div>
   </a>
-  <a href="#">
+  <a href="#" class="cardColor3">
     <div class="meritCard">
       <h3>
         <span>Questo è</span>
