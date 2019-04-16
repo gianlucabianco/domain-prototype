14f1c70b2fef1f6dddd9c8ac7f2e57b87c091400
gianlucabianco <pi.gs.bianco@gmail.com>
2019-04-16 17:22:54 +0200

    form responsive

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -6,14 +6,15 @@
       <p>
         Se hai in mente di avviare un progetto, pensi che il mio aiuto ti possa essere di qualche utilità o più semplicemente vuoi solo lasciarmi un saluto, ti prego di scrivermi due righe.
       </p>
-      <div class="form-group">
-        <input type="text" name="contactName" class="form-control" placeholder="Nome">
-        <input type="text" name="contactEmail" class="form-control" placeholder="Email">
-        <textarea rows="10" name="contactMessage" class="form-control" placeholder="Inserisci qui il tuo messaggio"></textarea>
-        <!-- rimuovere ".tempAlert" appena si mette in produzione la funzionalità di riferimento: -->
-        <input type="submit" value="Invia messaggio" class="form-control tempAlert">
+      <div class="testWrapper">
+        <div class="form-group">
+          <input type="text" name="contactName" class="form-control" placeholder="Nome">
+          <input type="text" name="contactEmail" class="form-control" placeholder="Email">
+          <textarea rows="10" name="contactMessage" class="form-control" placeholder="Inserisci qui il tuo messaggio"></textarea>
+          <!-- rimuovere ".tempAlert" appena si mette in produzione la funzionalità di riferimento: -->
+          <input type="submit" value="Invia messaggio" class="form-control tempAlert">
+        </div>
       </div>
     </div>
-
   </div>
 </div>
